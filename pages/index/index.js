244037c253a41c575a1a67527142df89277887b3
HHZ <676964236@qq.com>
2018-03-11 09:42:34 +0800

    311UL

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,27 +1,71 @@
 Page({
   data: {
+    Icon:[],
+    uploadTimes:0,
+    hiden:true,
     topStories: [
       'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=684276477,2474013616&fm=27&gp=0.jpg',
       'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1623529198,932220174&fm=27&gp=0.jpg',
       'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1446989525,2907093786&fm=27&gp=0.jpg',
       'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1192368817,540312063&fm=27&gp=0.jpg',
     ],
-    storyList: [
-      'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=684276477,2474013616&fm=27&gp=0.jpg',
-      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1623529198,932220174&fm=27&gp=0.jpg',
-      'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1446989525,2907093786&fm=27&gp=0.jpg',
-      'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1192368817,540312063&fm=27&gp=0.jpg',
-    ],
-    story_attetion:
-    [
-
+    getStorydata:[
 
     ],
-    story_location: [
-      ,
-    ]
+    storyList:[],
   },
+  LM: function (e) {
+    
+    var that = this;
+    console.log('555')
+    that.setData({
+      hiden:true
+    })
+    if(that.data.uploadTimes>=5)
+    {
+      that.setData({
+        hiden:false
+      });
+    } 
+    else{
+      console.log('$$$$$$$$$$$$$$$$$4')
+      wx.request({url: 'https://brightasdream.cn/uploadImage/handlepage',
+        data:{
+        'page': that.data.uploadTimes,
+      },
 
+  success: function (res) {
+    console.log(res.data);
+    that.setData({
+      storyList: res.data,
+    });
+    var l = res.data;
+    var p = new Array(l.length);
+    for (var j = 0; j < l.length; j++) {
+      var userId = l[j].uploadId.split("$");
+      var t = l[j].image_path.split("&");//取出图片名
+      t.pop();//删除最后一个空白元素
+      p[j] = new Array(t.length);
+      p[j] = t;
+      for (var i = 0; i < t.length; i++) {
+        p[j][i] = 'https://brightasdream.cn/uploadImage/upload/' + userId[0] + '/' + l[j].uploadId + '/' + p[j][i];//得到服务器上的图片的路劲
+      }
+      that.setData(
+        {
+          Icon: p,
+        }
+      )
+    }
+  }
+})
+that.setData({
+      uploadTimes: that.data.uploadTimes + 1
+})
+
+
+    }
+
+  },
   /**
    * 这里处理滚动事件处理
    */
@@ -32,8 +76,43 @@ Page({
 
   onLoad: function (options) {
     // 页面初始化 options为页面跳转所带来的参数
+    var that = this;
     wx.request({
-      url: 'https://brightasdream.cn'
+      url: 'https://brightasdream.cn/uploadImage/handlepage',
+      data:{
+        page:'1',
+      },
+
+      success:function(res){
+        console.log(res.data);
+        that.setData({
+          storyList:res.data,
+        });
+        var l = res.data;
+        var p = new Array(l.length);
+        for (var j = 0; j < l.length; j++) {
+          var userId = l[j].uploadId.split("$");
+          var t = l[j].image_path.split("&");//取出图片名
+          t.pop();//删除最后一个空白元素
+          p[j] = new Array(t.length);
+          p[j] = t;
+          for (var i = 0; i < t.length; i++) {
+            p[j][i] = 'https://brightasdream.cn/uploadImage/upload/' + userId[0] + '/' + l[j].uploadId + '/' + p[j][i];//得到服务器上的图片的路劲
+          }
+          that.setData(
+            {
+              Icon: p,
+            }
+
+    
+          )
+          console.log('**')
+          console.log(that.data.Icon)
+        }
+      }
+    })
+    that.setData({
+    uploadTimes:that.data.uploadTimes+1
     })
   },
   onReady: function () {
@@ -47,5 +126,9 @@ Page({
   },
   onUnload: function () {
     // 页面关闭
+  },
+  loadMore: function(e){
+    var that=this;
+    
   }
 })
